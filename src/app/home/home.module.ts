a58aa56da9a6401c0e577b800421a67e977c21a9
Satyam Jasoliya <satyamjasoliya005@gmail.com>
2022-03-30 21:16:06 +0530

    product step 2 component delete

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { ProductComponent } from './product/product.component';
 import { ProductAddStep1Component } from './add-product/product-add-step1/product-add-step1.component';
-import { ProductAddStep2Component } from './add-product/product-add-step2/product-add-step2.component';
 import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductComponent,
-    ProductAddStep1Component,
-    ProductAddStep2Component
+    ProductAddStep1Component
   ],
   imports: [
     CommonModule,
