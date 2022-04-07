35a93178e472f6c0f38cbaa84a611c72da9787b9
Satyam Jasoliya <satyamjasoliya005@gmail.com>
2022-04-07 23:46:46 +0530

    dialog changes

--- src/app/home/product/product.component.ts
+++ src/app/home/product/product.component.ts
@@ -1,44 +1,25 @@
 //External imports
 import { Component, OnInit } from '@angular/core';
-import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { MessageService } from 'primeng/api';
-//Internal imports
-import { AddProductComponent } from './../add-product/add-product.component';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.less'],
-  providers: [DialogService, MessageService]
+  providers: [MessageService]
 })
 
 export class ProductComponent implements OnInit {
-  ref: DynamicDialogRef | any;
+  public productName : string = '';
+  public isOpenDialog = false;
 
-  constructor(public dialogService: DialogService, public messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   public addProductStepOne(productName: string) {
-    this.ref = this.dialogService.open(AddProductComponent, {
-      header:'Add Product to ' +productName,
-      width: '41.25rem',
-      height:'41.875rem',
-      baseZIndex: 10000,
-      dismissableMask: true
-    });
-
-    this.ref.onClose.subscribe((product: any) => {
-      if (product) {
-        //dialog data receive here
-      }
-    });
+    this.productName = productName;
+    this.isOpenDialog = true;
   }
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy() {
-    if (this.ref) {
-      this.ref.close();
-    }
   }
 }
